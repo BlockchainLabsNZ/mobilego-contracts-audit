d6e8ba7deff55121dcfc8d09589467a3c4cd9ae0
Matt <sifrawr@gmail.com>
2017-10-30 14:53:35 +1300

    more tests, making sure betting function is working correctly

--- test/functionTests.js
+++ test/functionTests.js
@@ -55,115 +55,171 @@ contract('Function Tests', async function ([owner, better, provider, existing_pr
     assert.equal(new_union[0], provider);
   });
 
-  describe('lockable functions', async function () {
+  describe('setup union and event', async function () {
     const existing_union = web3.fromAscii("Test Union 2");
-    const existing_event = web3.fromAscii("Test Event 2");
+    const existing_event = web3.fromAscii("Event option 1");
+    const existing_event2 = web3.fromAscii("Event option 2");
     beforeEach(async function () {
       await DeSports.createUnion(existing_union, { from: existing_provider });
       await DeSports.createEvent(existing_event, existing_union, { from: existing_provider });
-      await DeSports.changeContractStatus(true);
-      assert.equal(await DeSports.contractLocked(), true, "Contract status should be locked");
+      await DeSports.createEvent(existing_event2, existing_union, { from: existing_provider });
     });
 
-    it('createUnion should be lockable', async function () {
-      await assertFail(async () => { await DeSports.createUnion(test_union) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.createUnion(test_union);
-      const event = logs.find(e => e.event === 'UnionCreation');
-      expect(event).to.exist;
-      let new_union = await DeSports.unions(test_union);
-      expect(new_union).to.exist;
+    describe('test betting', async function () {
+      beforeEach(async function () {
+        await DeSports.setQuota(existing_union, 0, new BigNumber("20000000000"), { from: existing_provider });
+        await DeSports.fundUnion(existing_union, 1000, { from: existing_provider });
+        await DeSports.startBetting(existing_union, { from: existing_provider });
+      });
+
+      it('betting shouldnt be valid after a union is resolved', async function () {
+        await DeSports.resolveUnion(existing_union, 0, { from: existing_provider });
+        let { logs } = await DeSports.bet(existing_union, 0, 10, new BigNumber("20000000000"), { from: better });
+        let event = logs.find(e => e.event === 'Bet');
+        expect(event).to.not.exist;
+      });
+
+      it('betting should fail if the quota is entered incorrectly', async function () {
+        let { logs } = await DeSports.bet(existing_union, 0, 10, new BigNumber("20000000001"), { from: better });
+        let event = logs.find(e => e.event === 'Bet');
+        expect(event).to.not.exist;
+      });
+
+      it('lockbetting should halt betting', async function () {
+        await DeSports.lockBetting(existing_union, true, { from: existing_provider });
+        let { logs } = await DeSports.bet(existing_union, 0, 10, new BigNumber("20000000000"), { from: better });
+        let event = logs.find(e => e.event === 'Bet');
+        expect(event).to.not.exist;
+      });
+
+      // it('only the person who placed the bet, can claim the bet', async function () {
+      // });
+
+      it('a bet should return nothing after an unsuccesful bet', async function () {
+        await DeSports.bet(existing_union, 0, 10, new BigNumber("20000000000"), { from: better });
+        await DeSports.resolveUnion(existing_union, 1, { from: existing_provider });
+        let { logs } = await DeSports.claimBet(existing_union, 0, { from: better });
+        let event = logs.find(e => e.event === 'BetClaim');
+        expect(event).to.exist;
+        assert.equal(event['args']['amount'].toNumber(), 0);
+      });
+
+      it('a bet should return a predictable amount after a success', async function () {
+        await DeSports.bet(existing_union, 0, 10, new BigNumber("20000000000"), { from: better });
+        await DeSports.resolveUnion(existing_union, 0, { from: existing_provider });
+        let { logs } = await DeSports.claimBet(existing_union, 0, { from: better });
+        let event = logs.find(e => e.event === 'BetClaim');
+        expect(event).to.exist;
+        assert.equal(event['args']['amount'].toNumber(), 20);
+      });
     });
 
-    it('fundUnion should be lockable', async function () {
-      await assertFail(async () => { await DeSports.fundUnion(existing_union, 123, { from: existing_provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.fundUnion(existing_union, 123, { from: existing_provider });
-      const event = logs.find(e => e.event === 'UnionFunding');
-      expect(event).to.exist;
-      let union = await DeSports.unions(existing_union);
-      // union[1] == uint providerFund;
-      assert.equal(union[1], 123, "The provider fund should be increased by 123");
-    });
+    describe('lockable functions', async function () {
+      beforeEach(async function () {
+        await DeSports.changeContractStatus(true);
+        assert.equal(await DeSports.contractLocked(), true, "Contract status should be locked");
+      });
 
-    it('setFee should be lockable', async function () {
-      await assertFail(async () => { await DeSports.setFee(123, { from: provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.setFee(123, { from: provider });
-      const event = logs.find(e => e.event === 'Fee');
-      expect(event).to.exist;
-      let provider_data = await DeSports.providers(provider);
-      assert.equal(provider_data[0].toNumber(), 123);
-    });
-
-    it('createEvent should be lockable', async function () {
-      await assertFail(async () => { await DeSports.createEvent(test_event, existing_union, { from: existing_provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.createEvent(test_event, existing_union, { from: existing_provider });
-      const event = logs.find(e => e.event === 'EventCreation');
-      expect(event).to.exist;
-    });
-
-    it('startBetting should be lockable', async function () {
-      await assertFail(async () => { await DeSports.startBetting(existing_union, { from: existing_provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.startBetting(existing_union, { from: existing_provider });
-      const event = logs.find(e => e.event === 'BettingStarted');
-      expect(event).to.exist;
-    });
-
-    it('setQuota should be lockable', async function () {
-      await assertFail(async () => { await DeSports.setQuota(existing_union, 0, 10000000001, { from: existing_provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.setQuota(existing_union, 0, new BigNumber("10000000001"), { from: existing_provider });
-      const event = logs.find(e => e.event === 'Quota');
-      expect(event).to.exist;
-    });
-
-    it('setQuotas should be lockable', async function () {
-      await assertFail(async () => { await DeSports.setQuotas(existing_union, [, new BigNumber("10000000001")], { from: existing_provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.setQuotas(existing_union, [new BigNumber("10000000001")], { from: existing_provider });
-      const event = logs.find(e => e.event === 'Quotas');
-      expect(event).to.exist;
-    });
-
-    it('lockBetting should be lockable', async function () {
-      await assertFail(async () => { await DeSports.lockBetting(existing_union, true, { from: existing_provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.lockBetting(existing_union, true, { from: existing_provider });
-      const event = logs.find(e => e.event === 'BettingLock');
-      expect(event).to.exist;
-    });
-
-    it('resolveUnion should be lockable', async function () {
-      await assertFail(async () => { await DeSports.resolveUnion(existing_union, 0, { from: existing_provider }) }, "Function should not be callable while locked");
-      await DeSports.changeContractStatus(false);
-      const { logs } = await DeSports.resolveUnion(existing_union, 0, { from: existing_provider });
-      const event = logs.find(e => e.event === 'UnionResolution');
-      expect(event).to.exist;
-    });
-
-    describe('user functions (lockable)', async function () {
-      it('associateAddresses should be lockable', async function () {
-        await assertFail(async () => { await DeSports.associateAddresses(0x1, 0x2, { from: existing_provider }) }, "Function should not be callable while locked");
+      it('createUnion should be lockable', async function () {
+        await assertFail(async () => { await DeSports.createUnion(test_union) }, "Function should not be callable while locked");
         await DeSports.changeContractStatus(false);
-        const { logs } = await DeSports.associateAddresses(0x1, 0x2, { from: existing_provider });
-        const event = logs.find(e => e.event === 'AddressesAssociation');
+        const { logs } = await DeSports.createUnion(test_union);
+        const event = logs.find(e => e.event === 'UnionCreation');
+        expect(event).to.exist;
+        let new_union = await DeSports.unions(test_union);
+        expect(new_union).to.exist;
+      });
+
+      it('fundUnion should be lockable', async function () {
+        await assertFail(async () => { await DeSports.fundUnion(existing_union, 123, { from: existing_provider }) }, "Function should not be callable while locked");
+        await DeSports.changeContractStatus(false);
+        const { logs } = await DeSports.fundUnion(existing_union, 123, { from: existing_provider });
+        const event = logs.find(e => e.event === 'UnionFunding');
+        expect(event).to.exist;
+        let union = await DeSports.unions(existing_union);
+        // union[1] == uint providerFund;
+        assert.equal(union[1], 123, "The provider fund should be increased by 123");
+      });
+
+      it('setFee should be lockable', async function () {
+        await assertFail(async () => { await DeSports.setFee(123, { from: provider }) }, "Function should not be callable while locked");
+        await DeSports.changeContractStatus(false);
+        const { logs } = await DeSports.setFee(123, { from: provider });
+        const event = logs.find(e => e.event === 'Fee');
+        expect(event).to.exist;
+        let provider_data = await DeSports.providers(provider);
+        assert.equal(provider_data[0].toNumber(), 123);
+      });
+
+      it('createEvent should be lockable', async function () {
+        await assertFail(async () => { await DeSports.createEvent(test_event, existing_union, { from: existing_provider }) }, "Function should not be callable while locked");
+        await DeSports.changeContractStatus(false);
+        const { logs } = await DeSports.createEvent(test_event, existing_union, { from: existing_provider });
+        const event = logs.find(e => e.event === 'EventCreation');
         expect(event).to.exist;
       });
 
-      it('bet should be lockable', async function () {
-        // Unlock the contract to start betting or else bet will always fail
+      it('startBetting should be lockable', async function () {
+        await assertFail(async () => { await DeSports.startBetting(existing_union, { from: existing_provider }) }, "Function should not be callable while locked");
         await DeSports.changeContractStatus(false);
-        await DeSports.startBetting(existing_union, { from: existing_provider });
-        await DeSports.setQuota(existing_union, 0, new BigNumber("10000000001"), { from: existing_provider });
-        await DeSports.changeContractStatus(true);
-        await assertFail(async () => { await DeSports.bet(existing_union, 0, 10, new BigNumber("10000000001"), { from: better }) }, "Function should not be callable while locked");
-        await DeSports.changeContractStatus(false);
-        const { logs } = await DeSports.bet(existing_union, 0, 10, new BigNumber("10000000001"), { from: better });
-        const event = logs.find(e => e.event === 'Bet');
+        const { logs } = await DeSports.startBetting(existing_union, { from: existing_provider });
+        const event = logs.find(e => e.event === 'BettingStarted');
         expect(event).to.exist;
+      });
+
+      it('setQuota should be lockable', async function () {
+        await assertFail(async () => { await DeSports.setQuota(existing_union, 0, 10000000001, { from: existing_provider }) }, "Function should not be callable while locked");
+        await DeSports.changeContractStatus(false);
+        const { logs } = await DeSports.setQuota(existing_union, 0, new BigNumber("10000000001"), { from: existing_provider });
+        const event = logs.find(e => e.event === 'Quota');
+        expect(event).to.exist;
+      });
+
+      it('setQuotas should be lockable', async function () {
+        await assertFail(async () => { await DeSports.setQuotas(existing_union, [new BigNumber("10000000001"), new BigNumber("10000000002")], { from: existing_provider }) }, "Function should not be callable while locked");
+        await DeSports.changeContractStatus(false);
+        const { logs } = await DeSports.setQuotas(existing_union, [new BigNumber("10000000001"), new BigNumber("10000000002")], { from: existing_provider });
+        const event = logs.find(e => e.event === 'Quotas');
+        expect(event).to.exist;
+      });
+
+      it('lockBetting should be lockable', async function () {
+        await assertFail(async () => { await DeSports.lockBetting(existing_union, true, { from: existing_provider }) }, "Function should not be callable while locked");
+        await DeSports.changeContractStatus(false);
+        const { logs } = await DeSports.lockBetting(existing_union, true, { from: existing_provider });
+        const event = logs.find(e => e.event === 'BettingLock');
+        expect(event).to.exist;
+      });
+
+      it('resolveUnion should be lockable', async function () {
+        await assertFail(async () => { await DeSports.resolveUnion(existing_union, 0, { from: existing_provider }) }, "Function should not be callable while locked");
+        await DeSports.changeContractStatus(false);
+        const { logs } = await DeSports.resolveUnion(existing_union, 0, { from: existing_provider });
+        const event = logs.find(e => e.event === 'UnionResolution');
+        expect(event).to.exist;
+      });
+
+      describe('user functions (lockable)', async function () {
+        it('associateAddresses should be lockable', async function () {
+          await assertFail(async () => { await DeSports.associateAddresses(0x1, 0x2, { from: existing_provider }) }, "Function should not be callable while locked");
+          await DeSports.changeContractStatus(false);
+          const { logs } = await DeSports.associateAddresses(0x1, 0x2, { from: existing_provider });
+          const event = logs.find(e => e.event === 'AddressesAssociation');
+          expect(event).to.exist;
+        });
+
+        it('bet should be lockable', async function () {
+          // Unlock the contract to start betting or else bet will always fail
+          await DeSports.changeContractStatus(false);
+          await DeSports.startBetting(existing_union, { from: existing_provider });
+          await DeSports.setQuota(existing_union, 0, new BigNumber("10000000001"), { from: existing_provider });
+          await DeSports.changeContractStatus(true);
+          await assertFail(async () => { await DeSports.bet(existing_union, 0, 10, new BigNumber("10000000001"), { from: better }) }, "Function should not be callable while locked");
+          await DeSports.changeContractStatus(false);
+          const { logs } = await DeSports.bet(existing_union, 0, 10, new BigNumber("10000000001"), { from: better });
+          const event = logs.find(e => e.event === 'Bet');
+          expect(event).to.exist;
+        });
       });
     });
   });
